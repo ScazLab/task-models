d08844447b7cdaf84382740efe2f57ef21514461
Olivier Mangin <olivier@mangin.com>
2016-08-22 10:56:02 -0400

    Moves hold_and_release example POMDP to module.

---
+++ hold_and_release.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from lib.pomdp import POMDP
+
+
+def hold_and_release_pomdp(cost_wait, cost_failure, cost_communication,
+                           proba_finish=.8):
+    start = np.array([1, 0, 0])
+    T = np.zeros((3, 3, 3))
+    T[:, 0, :] = [proba_finish, 1 - proba_finish, 0.],  # if human has not
+    #                                                   # finished
+    T[:, 2, :] = [0., 0., 1.],   # if human and robot have finished
+    T[:, 1, :] = [[0., 1., 0.],  # if human has finished robot needs to act
+                  [0., 0., 1.],
+                  [0., 1., 0.]]
+    O = np.zeros((3, 3, 3))
+    O[0, :, :] = [1., 0., 0.]         # nothing observed on wait
+    O[[1, 2], :, :] = [[0., 1., 0.],  # on com or physical human progress is
+                       [0., 0., 1.],  # observed
+                       [1., 0., 0.]]
+    R = np.zeros((3, 3, 3, 3))
+    R[0, :, :, :] = cost_wait  # Cost for waiting is constant
+    R[1, 0, :, :] = [[cost_failure], [0], [0]]  # On physical action, failure
+    #                                           # if human unfinished
+    R[2, :, :, :] = cost_communication  # Constant cost for communication
+    return POMDP(T, O, R, start, 1,
+                 actions=['wait', 'physical', 'communicate'],
+                 states=['zero', 'one', 'final'],
+                 observations=['nothing', 'doing', 'done'],
+                 values='cost')
--- samples/hold_and_release.py
+++ samples/hold_and_release.py
@@ -1,35 +1,15 @@
 import numpy as np
 
-from htm.lib.pomdp import POMDP, GraphPolicyRunner
+from htm.lib.pomdp import GraphPolicyRunner
+from htm.hold_and_release import hold_and_release_pomdp
 
 
 C_WAIT = 1.
 C_FAILURE = 5.
 C_COMMUNICATION = 2.
 
-start = np.array([1, 0, 0])
-T = np.zeros((3, 3, 3))
-T[:, 0, :] = [.8, .2, 0.],   # if human has not finished
-T[:, 2, :] = [0., 0., 1.],   # if human and robot have finished
-T[:, 1, :] = [[0., 1., 0.],  # if human has finished robot needs to act
-              [0., 0., 1.],
-              [0., 1., 0.]]
-O = np.zeros((3, 3, 3))
-O[0, :, :] = [1., 0., 0.]         # nothing observed on wait
-O[[1, 2], :, :] = [[0., 1., 0.],  # on com or physical human progress is
-                   [0., 0., 1.],  # observed
-                   [1., 0., 0.]]
-R = np.zeros((3, 3, 3, 3))
-R[0, :, :, :] = C_WAIT  # Cost for waiting is constant
-R[1, 0, :, :] = [[C_FAILURE], [0], [0]]  # On phy., failure if human unfinished
-R[2, :, :, :] = C_COMMUNICATION  # Constant cost for communication
 
-
-p = POMDP(T, O, R, start, 1,
-          actions=['wait', 'physical', 'communicate'],
-          states=['zero', 'one', 'final'],
-          observations=['nothing', 'doing', 'done'],
-          values='cost')
+p = hold_and_release_pomdp(C_WAIT, C_FAILURE, C_COMMUNICATION)
 pgr = GraphPolicyRunner(p.solve())
 pgr.gp.print()
 for i in range(10):
