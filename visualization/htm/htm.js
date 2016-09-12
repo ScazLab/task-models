b6edc9757324cb6b4719f277e4fe404204f7c45b
Alessandro Roncone <alecive87@gmail.com>
2016-09-12 15:41:30 -0400

    [visualization][htm] moved to single-quote strings as the other js files

--- visualization/htm/htm.js
+++ visualization/htm/htm.js
@@ -1,5 +1,5 @@
 // ************** Generate the tree diagram  *****************
-var jsonfile = "json/icra.json";
+var jsonfile = 'json/icra.json';
 
 var width  = 1560,
     height = 700;
@@ -19,11 +19,11 @@ var tree = d3.layout.tree()
 var diagonal = d3.svg.diagonal()
                  .projection(function(d) { return [d.x+rectW/2, d.y+rectH/2]; });
 
-var svg = d3.select("#tree-container").append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .call(zm = d3.behavior.zoom().scaleExtent([1,3]).on("zoom", redraw)).append("g")
-            .attr("transform", "translate(" + (width-rectW)/2 + "," + 20 + ")");
+var svg = d3.select('#tree-container').append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .call(zm = d3.behavior.zoom().scaleExtent([1,3]).on('zoom', redraw)).append('g')
+            .attr('transform', 'translate(' + (width-rectW)/2 + ',' + 20 + ')');
 
 //necessary so that zoom knows where to zoom and unzoom from
 zm.translate([(width-rectW)/2, 20]);
@@ -56,65 +56,65 @@ function update(source) {
   nodes.forEach(function(d) { d.y = d.depth * 100; });
 
   // Declare the nodes...
-  var node = svg.selectAll("g.node")
+  var node = svg.selectAll('g.node')
                 .data(nodes, function(d) { return d.id; }); // { return d.id || (d.id = ++i); });
 
   // Enter the nodes.
   var nodeEnter = node.enter()
-                      .append("g")
-                      .attr("class", function(d) {
-                        var res="node";
-                        if (d.attributes) {res=res+" "+d.attributes.join(" ");}
-                        if (d._children)  {res=res+" collapsed";}
+                      .append('g')
+                      .attr('class', function(d) {
+                        var res='node';
+                        if (d.attributes) {res=res+' '+d.attributes.join(' ');}
+                        if (d._children)  {res=res+' collapsed';}
                         return res; })
-                      .attr("transform", function(d) { return "translate(" + source.x0 + "," + source.y0 + ")"; })
-                      .on("click", click);
+                      .attr('transform', function(d) { return 'translate(' + source.x0 + ',' + source.y0 + ')'; })
+                      .on('click', click);
 
-  nodeEnter.append("rect")
-           .attr("width", rectW)
-           .attr("height", rectH)
-           .attr("class", "label");
+  nodeEnter.append('rect')
+           .attr('width', rectW)
+           .attr('height', rectH)
+           .attr('class', 'label');
 
-  nodeEnter.append("text")
-           .attr("x", rectW / 2)
-           .attr("y", rectH / 2)
-           .attr("dy", ".35em")
-           .attr("text-anchor", "middle")
+  nodeEnter.append('text')
+           .attr('x', rectW / 2)
+           .attr('y', rectH / 2)
+           .attr('dy', '.35em')
+           .attr('text-anchor', 'middle')
            .text(function (d) { return d.name; });
 
   // Add combination if there is a combination and the node is not collapsed
   nodeCombination = nodeEnter.filter(function(d,i){ return d.combination; }) // && !d._children && d.children; })
-                             .append("g")
-                             .attr("class","combination");
+                             .append('g')
+                             .attr('class','combination');
 
-  nodeCombination.append("rect")
-                 .attr("width", 36)
-                 .attr("height", 36)
-                 .attr("x", (rectW-36)/2)
-                 .attr("y", rectH + 1);
+  nodeCombination.append('rect')
+                 .attr('width', 36)
+                 .attr('height', 36)
+                 .attr('x', (rectW-36)/2)
+                 .attr('y', rectH + 1);
 
-  nodeCombination.append("text")
-                 .attr("x", rectW / 2)
-                 .attr("y", rectH / 2 - 12)
-                 .attr("dy", "3.5em")
-                 .attr("text-anchor", "middle")
+  nodeCombination.append('text')
+                 .attr('x', rectW / 2)
+                 .attr('y', rectH / 2 - 12)
+                 .attr('dy', '3.5em')
+                 .attr('text-anchor', 'middle')
                  .text(function (d) {
-                    if (d.combination=="Parallel") {return "||";}
-                    if (d.combination=="Sequence") {return "→";}
-                    if (d.combination=="Alternative") {return "v";}
-                    return ""
+                    if (d.combination=='Parallel') {return '||';}
+                    if (d.combination=='Sequence') {return '→';}
+                    if (d.combination=='Alternative') {return 'v';}
+                    return ''
                   });
 
   // Transition nodes to their new position.
   var nodeUpdate = node.transition()
                        .duration(duration)
-                       .attr("transform", function (d) {return "translate(" + d.x + "," + d.y + ")";});
+                       .attr('transform', function (d) {return 'translate(' + d.x + ',' + d.y + ')';});
 
-  var gUpdate = nodeUpdate.attr("class", function(d) {
-                            var cl=d3.select(this).attr("class");
+  var gUpdate = nodeUpdate.attr('class', function(d) {
+                            var cl=d3.select(this).attr('class');
                             // console.log(cl,d);
-                            if (d._children) { if (cl.indexOf(" collapsed")==-1) { return cl+" collapsed"; } }
-                            else { if (cl.indexOf(" collapsed")!=-1) return cl.replace(" collapsed",""); }
+                            if (d._children) { if (cl.indexOf(' collapsed')==-1) { return cl+' collapsed'; } }
+                            else { if (cl.indexOf(' collapsed')!=-1) return cl.replace(' collapsed',''); }
                             return cl;
                           });
 
@@ -122,20 +122,20 @@ function update(source) {
   // Transition exiting nodes to the parent's new position.
   var nodeExit = node.exit().transition()
                             .duration(duration)
-                            .attr("transform", function (d) {return "translate(" + source.x + "," + source.y + ")";})
+                            .attr('transform', function (d) {return 'translate(' + source.x + ',' + source.y + ')';})
                             .remove();
 
   // Declare the links...
-  var link = svg.selectAll("path.link")
+  var link = svg.selectAll('path.link')
                 .data(links, function(d) { return d.target.id; });
 
   // console.log(link);
   // Enter any new links at the parent's previous position.
-  link.enter().insert("path", "g")
-      .attr("class", "link")
-      .attr("x", rectW / 2)
-      .attr("y", rectH / 2)
-      .attr("d", function (d) {
+  link.enter().insert('path', 'g')
+      .attr('class', 'link')
+      .attr('x', rectW / 2)
+      .attr('y', rectH / 2)
+      .attr('d', function (d) {
         var o = {
             x: source.x0,
             y: source.y0
@@ -146,12 +146,12 @@ function update(source) {
   // Transition links to their new position.
   link.transition()
       .duration(duration)
-      .attr("d", diagonal);
+      .attr('d', diagonal);
 
   // Transition exiting nodes to the parent's new position.
   link.exit().transition()
       .duration(duration)
-      .attr("d", function (d) {
+      .attr('d', function (d) {
         var o = {
             x: source.x,
             y: source.y
@@ -170,8 +170,8 @@ function update(source) {
 
 //Redraw for zoom
 function redraw() {
-  //console.log("here", d3.event.translate, d3.event.scale);
-  svg.attr("transform",
-      "translate(" + d3.event.translate + ")"
-      + " scale(" + d3.event.scale + ")");
+  //console.log('here', d3.event.translate, d3.event.scale);
+  svg.attr('transform',
+      'translate(' + d3.event.translate + ')'
+      + ' scale(' + d3.event.scale + ')');
 }
