36bab64b174ce5d1d347c0900bb189bd8f182851
Olivier Mangin <olivier@mangin.com>
2016-09-12 14:58:04 -0400

    [task2pomdp] Adds loop in POMDP.
    Goes back to init when task done (observes 'done').

--- samples/htm2pomdp.py
+++ samples/htm2pomdp.py
@@ -1,50 +1,54 @@
 import os
 
 from htm.task import (HierarchicalTask, SequentialCombination,
-                      AlternativeCombination, LeafCombination)
+                      AlternativeCombination, LeafCombination,
+                      ParallelCombination)
 from htm.task_to_pomdp import CollaborativeAction, HTMToPOMDP
+# Costs
+T_WAIT = 1.
+T_COMM = 2.
+C_INTR = 1.
+C_ERR = 5.
+R_END = 100
 
 ## Define the task
 mount_central = SequentialCombination([
-    LeafCombination(CollaborativeAction('Get central frame', (10., 3., 5.))),
-    LeafCombination(CollaborativeAction('Snap central frame', (3., 10., 5.)))],
+    LeafCombination(CollaborativeAction(
+        'Get central frame', (10., 3., C_ERR))),
+    LeafCombination(CollaborativeAction(
+        'Snap central frame', (3., 10., C_ERR)))],
     name='Mount central frame')
-mount_legs = AlternativeCombination([
-    SequentialCombination(
-        [LeafCombination(CollaborativeAction(
-            'Get leg {} ({} first)'.format(sides[0], sides[0]),
-            (10., 3., 5.))),
-         LeafCombination(CollaborativeAction(
-             'Snap leg {}'.format(sides[1], sides[0]), (10., 3., 5.)))
-         ],
-        name='Mount legs ({} first)'.format(sides[0]))
-    for sides in [('left', 'right'), ('right', 'left')]
-    ], name='Mount legs')
-# Use a simpler one until Alternative to POMDP is implemented
+#mount_legs = ParallelCombination([
 mount_legs = SequentialCombination([
-    LeafCombination(CollaborativeAction('Get left leg', (10., 3., 5.))),
-    LeafCombination(CollaborativeAction('Snap left leg', (3., 10., 5.))),
-    LeafCombination(CollaborativeAction('Get right leg', (10., 3., 5.))),
-    LeafCombination(CollaborativeAction('Snap right leg', (3., 10., 5.))),
+    SequentialCombination([
+        LeafCombination(CollaborativeAction(
+            'Get left leg', (10., 3., C_ERR))),
+        LeafCombination(CollaborativeAction(
+            'Snap left leg', (3., 10., C_ERR))),
+        ], name='Mount left leg'),
+    SequentialCombination([
+        LeafCombination(CollaborativeAction(
+            'Get right leg', (10., 3., C_ERR))),
+        LeafCombination(CollaborativeAction(
+            'Snap right leg', (3., 10., C_ERR))),
+        ], name='Mount right leg'),
     ],
     name='Mount legs')
 mount_top = SequentialCombination([
-    LeafCombination(CollaborativeAction('Get top', (10., 3., 5.))),
-    LeafCombination(CollaborativeAction('Snap top', (3., 10., 5.)))],
+    LeafCombination(CollaborativeAction('Get top', (10., 3., C_ERR))),
+    LeafCombination(CollaborativeAction('Snap top', (3., 10., C_ERR)))],
     name='Mount top')
 
 chair_task = HierarchicalTask(root=SequentialCombination(
     [mount_central, mount_legs, mount_top], name='Mount chair'))
 
 ## Convert the task into a POMDP
-T_WAIT = 1.
-T_COMM = 2.
-C_INTR = 1.
 
-h2p = HTMToPOMDP(T_WAIT, T_COMM, C_INTR, False, False)
+h2p = HTMToPOMDP(T_WAIT, T_COMM, C_INTR, end_reward=R_END, loop=True)
 p = h2p.task_to_pomdp(chair_task)
+#p.discount = .99
 
-gp = p.solve(method='grid', n_iterations=1000)
+gp = p.solve(method='grid', n_iterations=1000, verbose=True)
 gp.dump_to(os.path.join(os.path.dirname(__file__),
                         '../visualization/policy/json/test.json'))
 
@@ -55,15 +59,31 @@ pol.save_trajectories_from_starts(
     os.path.join(os.path.dirname(__file__),
                  '../visualization/trajectories/json/trajectories.json'),
     horizon=10, indent=2)
+gp2 = pol.visit()
+gp2.dump_to(os.path.join(os.path.dirname(__file__),
+                         '../visualization/policy/json/from_beliefs.json'))
+
 
 from htm.plot import plot_beliefs
 import matplotlib.pyplot as plt
 
 plt.interactive(True)
+plt.close('all')
+
+
+def plot_values(values, actions, p):
+    b = values - values.min()
+    b /= b.max(-1)[:, None]
+    plot = plot_beliefs(b, states=p.states, xlabels_rotation=45,
+                        ylabels=["{}: {}".format(i, a)
+                                for i, a in enumerate(actions)])
+    plt.colorbar(plot)
+
+
 plt.figure()
-b = gp.values - gp.values.min()
-b /= b.max(-1)[:, None]
-plot = plot_beliefs(b, states=p.states, xlabels_rotation=45,
-                    ylabels=["{}: {}".format(i, a)
-                             for i, a in enumerate(gp.actions)])
-plt.colorbar(plot)
+plot_values(gp.values, gp.actions, p)
+plt.title('Policy values')
+
+plt.figure()
+plot_values(gp2.values, gp2.actions, p)
+plt.title('Belief trajectory')
