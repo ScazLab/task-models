556425ac6b6604ad5c51a3805b7f3023e41cea36
Alessandro Roncone <alecive87@gmail.com>
2016-09-13 13:13:57 -0400

    [visualization][htm] removed dependency from roslibjs in order to be ROS - indepenent

--- visualization/htm/htm.js
+++ visualization/htm/htm.js
@@ -203,13 +203,6 @@ function loadhtm(file)
     console.log('Pressed item: '+d.name+'\tdepth: '+d.depth+'\tattr: '+d.attributes);
     // console.log(tree.links(d).toString());
 
-    var message = new ROSLIB.Message({
-      data: d.name+' '+d.state
-    });
-
-    // And finally, publish.
-    elemPressed.publish(message);
-
     if (d.children) {
       d._children = d.children;
       d.children = null;
