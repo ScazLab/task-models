a7bb2663bf821493b82bf18790018dd5753c5c17
Olivier Mangin <olivier@mangin.com>
2017-07-19 17:51:30 -0400

    [samples] WIP on multitask.

--- samples/multitask_evaluation.py
+++ samples/multitask_evaluation.py
@@ -1,12 +1,24 @@
+import sys
 import time
 import logging
+import argparse
+
+import numpy as np
 
 from task_models.lib.pomcp import POMCPPolicyRunner, export_pomcp
 from task_models.task import (LeafCombination, AlternativeCombination,
                               SequentialCombination)
 from task_models.supportive import (SupportivePOMDP, AssembleLeg, SupportedAction,
                                     CONSUMES)
-from task_models.evaluation import simulate_one_evaluation
+from task_models.policy import PolicyRunner, PolicyLongSupportiveSequence
+from task_models.evaluation import evaluate
+
+
+parser = argparse.ArgumentParser(
+    description="Script to run evaluation job on the multitask problem")
+parser.add_argument('--debug', action='store_true',
+                    help='quick interactive run with dummy parameters')
+args = parser.parse_args(sys.argv[1:])
 
 
 def task_long_sequence(n):
@@ -14,111 +26,6 @@ def task_long_sequence(n):
     htm = SequentialCombination([LeafCombination(AssembleLeg(leg_i(i)))
                                  for i in range(n)])
     return htm
-
-
-# TODO: eventually merge with POMCPPolicyRunner
-
-class PolicyRunner(object):
-
-    def __init__(self, model):
-        self._model = model
-
-    @property
-    def model(self):
-        return self._model
-
-    @model.setter
-    def model(self, o):
-        raise Exception(o)
-
-    @property
-    def actions(self):
-        return self.model.actions
-
-    @property
-    def observations(self):
-        return self.model.observations
-
-    def reset(self):
-        self.history = []
-
-    def get_action(self):
-        """Needs to store action index as self._last_action."""
-        raise NotImplementedError
-
-    def step(self, observation):
-        if self._last_action is None:
-            raise ValueError('Unknown last action')
-        o = self.observations.index(observation)
-        self.history.extend([self._last_action, o])
-
-
-class PolicyLongSupportiveSequence(PolicyRunner):
-
-    class UnexpectedObservation(RuntimeError):
-        pass
-
-    def __init__(self, model):
-        super(PolicyLongSupportiveSequence, self).__init__(model)
-        self._a_next = self.actions.index('hold H')
-        self._a_wait = self.actions.index('wait')
-        self._o_none = self.observations.index('none')
-        self.n_tasks = self.model.n_htm_states - 2
-        self.tools = ['screws', 'screwdriver', 'joints']
-        self.reset()
-
-    def _a_bring(self, obj):
-        return self.actions.index('bring {}'.format(obj))
-
-    def _a_clean(self, obj):
-        return self.actions.index('clear {}'.format(obj))
-
-    def reset(self):
-        super(PolicyLongSupportiveSequence, self).reset()
-        self._to_bring = [self._a_bring(o) for o in self.tools]
-        self._to_clean = [self._a_clean(o) for o in self.tools]
-        self._needs_leg = True
-        self._last_action = None
-        self._n_done = 0
-
-    def get_action(self, iterations=None):
-        if len(self._to_bring) > 0:
-            a = self._to_bring[0]
-        elif self._n_done < self.n_tasks:
-            if self._needs_leg:
-                a = self._a_bring('leg')
-            else:
-                a = self._a_next
-        elif len(self._to_clean) > 0:
-            a = self._to_clean[0]
-        else:  # Final wait
-            a = self._a_wait
-        self._last_action = a
-        return self.actions[a]
-
-    def step(self, observation):
-        super(PolicyLongSupportiveSequence, self).step(observation)
-        unexpected = self.UnexpectedObservation('{} for action {}'.format(
-            observation, self.actions[self._last_action]))
-        if observation == 'fail':
-            pass
-        elif observation in ('none', 'not-found'):
-            if self._last_action in [self._a_bring(o) for o in self.tools]:
-                self._to_bring.remove(self._last_action)
-            elif self._last_action in [self._a_clean(o) for o in self.tools]:
-                self._to_clean.remove(self._last_action)
-            elif self._last_action == self._a_bring('leg'):
-                self._needs_leg = False
-            elif self._last_action == self._a_next and observation == 'none':
-                # Next subtask
-                self._n_done += 1
-                self._needs_leg = True
-            elif self._last_action == self._a_wait:
-                pass
-            else:
-                raise unexpected
-        else:
-            raise unexpected
 
 
 class CustomObjectAction(SupportedAction):
@@ -136,6 +43,22 @@ def task_alternative(n):
          for i in range(n)])
 
 
+class PolicySupportiveAlternatives(PolicyRunner):
+
+    def __init__(self, model):
+        super(PolicySupportiveAlternatives, self).__init__(model)
+        self._a_next = self.actions.index('hold H')
+        self._a_wait = self.actions.index('wait')
+        self._o_none = self.observations.index('none')
+        self.n_tasks = self.model.n_htm_states - 2
+        self.tools = ['screws', 'screwdriver', 'joints']
+        self.reset()
+
+
+def average_return(evaluations):
+    return np.average([x['return'] for x in evaluations])
+
+
 PARAM = {
     # Algorithm parameters
     'n_warmup': 1000,         # initial warmup exploration
@@ -145,14 +68,30 @@ PARAM = {
     'relative_explo': False,  # In this case use smaller exploration
     'belief_values': False,
     'n_particles': 150,
-    'horizon-length': 20,
+    'horizon-length': 50,
     'intermediate-rewards': False,
+    'n_subtasks': 20,
 }
 
+if args.debug:
+    PARAM['n_warmup'] = 2
+    PARAM['n_evaluations'] = 2
+    PARAM['iterations'] = 3
+    PARAM['n_particles'] = 20
+    PARAM['horizon-length'] = 2
 
-p = SupportivePOMDP(task_long_sequence(20))
+
+p = SupportivePOMDP(task_long_sequence(PARAM['n_subtasks']), discount=.95)
 p.p_preferences = [1.]
 p.p_change_preference = 0.
+# p.r_final = 10  # TODO move to parameter
+
+logging.basicConfig(level=logging.INFO)
+info = logging.info
+warn = logging.warning
+tostore = {'parameters': PARAM}
+info('Starting warmup')
+# POMCP policy
 pol = POMCPPolicyRunner(p, iterations=PARAM['iterations'],
                         horizon=PARAM['horizon-length'],
                         exploration=PARAM['exploration'],
@@ -160,13 +99,18 @@ pol = POMCPPolicyRunner(p, iterations=PARAM['iterations'],
                         belief_values=PARAM['belief_values'],
                         belief='particle',
                         belief_params={'n_particles': PARAM['n_particles']})
-pol = PolicyLongSupportiveSequence(p)
-
-logging.basicConfig(level=logging.INFO)
-info = logging.info
-info('Starting warmup')
 # Some initial exploration
 t_0 = time.time()
 pol.get_action(iterations=PARAM['n_warmup'])
-info('Warmup done in {}s.'.format(time.time() - t_0))
-simulate_one_evaluation(p, pol, max_horizon=1000, logger=info)
+tostore['t_warmup'] = time.time() - t_0
+info('Warmup done in {}s.'.format(tostore['t_warmup']))
+tostore['evaluations-pomcp'] = evaluate(p, pol, PARAM['n_evaluations'],
+                                        logger=info)
+warn('POMCP average return: {}'.format(
+    average_return(tostore['evaluations-pomcp'])))
+# Ad-hoc policy
+pol = PolicyLongSupportiveSequence(p)
+tostore['evaluations-adhoc'] = evaluate(p, pol, PARAM['n_evaluations'],
+                                        logger=info)
+warn('Ad-hoc average return: {}'.format(
+    average_return(tostore['evaluations-adhoc'])))
