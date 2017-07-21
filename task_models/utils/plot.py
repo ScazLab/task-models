f3cb98e48e71afb392e92a0b4240459610aaa10e
Olivier Mangin <olivier@mangin.com>
2017-07-21 19:46:10 -0400

    [utils/plot] Fix missing ticks

--- task_models/utils/plot.py
+++ task_models/utils/plot.py
@@ -135,7 +135,7 @@ def boxplot(x, **kwargs):
     if xticklabels is not None:
         ax.xaxis.set_ticklabels(xticklabels)
 
-    remove_chartjunk(ax, ['top', 'right', 'bottom'])
+    remove_chartjunk(ax, ['top', 'right', 'bottom'], ticks='x')
     linewidth = 0.75
 
     plt.setp(bp['boxes'], color=BLUE, linewidth=linewidth)
@@ -157,7 +157,7 @@ def pcolormesh(x, **kwargs):
     # Plot
     p = ax.pcolormesh(x, **kwargs)
     # Clear
-    remove_chartjunk(ax, ['top', 'right', 'bottom', 'left'])
+    remove_chartjunk(ax, ['top', 'right', 'bottom', 'left'], ticks='xy')
     # Set ticks
     if xticklabels:
         ax.set_xticks(np.arange(0.5, x.shape[1] + 0.5))
@@ -170,12 +170,14 @@ def pcolormesh(x, **kwargs):
     return p
 
 
-def remove_chartjunk(ax, spines, grid=None, ticklabels=None):
+def remove_chartjunk(ax, spines, grid=None, ticks=None, ticklabels=None):
     '''
     Removes "chartjunk", such as extra lines of axes and tick marks.
 
     If grid="y" or "x", will add a white grid at the "y" or "x" axes,
     respectively
+
+    If ticks="y" or "x", or ['x', 'y'] will remove ticks from that axis
 
     If ticklabels="y" or "x", or ['x', 'y'] will remove ticklabels from that
     axis
@@ -190,24 +192,13 @@ def remove_chartjunk(ax, spines, grid=None, ticklabels=None):
         if spine not in spines:
             ax.spines[spine].set_linewidth(0.5)
             # ax.spines[spine].set_color(almost_black)
-            #            ax.spines[spine].set_tick_params(color=almost_black)
-            # Check that the axes are not log-scale. If they are, leave the
-            # ticks because otherwise people assume a linear scale.
-    x_pos = set(['top', 'bottom'])
-    y_pos = set(['left', 'right'])
-    xy_pos = [x_pos, y_pos]
-    xy_ax_names = ['xaxis', 'yaxis']
+            # ax.spines[spine].set_tick_params(color=almost_black)
 
-    for ax_name, pos in zip(xy_ax_names, xy_pos):
-        axis = ax.__dict__[ax_name]
-        # axis.set_tick_params(color=almost_black)
-        if axis.get_scale() == 'log':
-            # if this spine is not in the list of spines to remove
-            for p in pos.difference(spines):
-                axis.set_ticks_position(p)
-                #                axis.set_tick_params(which='both', p)
-        else:
-            axis.set_ticks_position('none')
+    if ticks is not None:
+        if 'x' in ticks:
+            ax.xaxis.set_ticks_position('none')
+        if 'y' in ticks:
+            ax.yaxis.set_ticks_position('none')
 
     if grid is not None:
         for g in grid:
